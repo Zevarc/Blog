d17e7fe09adbae7437f40a96d66267c0bbc571a4
hongui <honguilee@163.com>
2026-03-17 21:39:01 +0800

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,40 +3,11 @@
   import favicon from '$lib/assets/favicon.svg'
   import '../styles/main.css';
   
-  // 页面过渡动画
-  onNavigate((navigation) => {
-    if (!document.startViewTransition) return;
-    
-    return new Promise((resolve) => {
-      document.startViewTransition(async () => {
-        resolve();
-        await navigation.complete;
-      });
-    });
-  });
+  const {children}=$props()
 </script>
 
 <svelte:head>
 		<link rel="icon" href={favicon} type="image/svg+xml" />
 </svelte:head>
 
-<style>
-  /* 页面过渡 */
-  :global(::view-transition-old(root)) {
-    animation: 0.4s ease-in-out both fade-out;
-  }
-  
-  :global(::view-transition-new(root)) {
-    animation: 0.4s ease-in-out both fade-in;
-  }
-  
-  @keyframes fade-out {
-    from { opacity: 1; transform: translateX(0); }
-    to { opacity: 0; transform: translateX(-30px); }
-  }
-  
-  @keyframes fade-in {
-    from { opacity: 0; transform: translateX(30px); }
-    to { opacity: 1; transform: translateX(0); }
-  }
-</style>
+{@render children()}
